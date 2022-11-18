0a48b9e334a17288a4dbd88c879c2b09c85beba6
Colin Armstrong <colinarms@gmail.com>
2022-11-17 20:50:49 -0800

    initial commit

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+//export const revalidate = 60; // revalidate this page every 60 seconds
+
+export const dynamic = "error";
+
+//export const dynamic = "error"
+
+export default function Page() {
+  return <div>Nice!</div>;
+}
